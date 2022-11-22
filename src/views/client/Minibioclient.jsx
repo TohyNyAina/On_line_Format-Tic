b06f5ab01493868346d115cc091ee6f1ee788a09
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2022-11-22 08:52:05 +0300

    Add files via upload
    Signed-off-by: Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>

---
+++ src/views/client/Minibioclient.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbarclient from '../../components/Navbars/navbarclient'
+
+export default function MinibioClient () {
+  return (
+    <div>
+        <Navbarclient/>
+        <h1>Ato no mipetraka ny portfolio</h1>
+    </div>
+  )
+}
